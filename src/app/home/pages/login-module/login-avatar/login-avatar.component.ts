8717530ccbcc1c2e9c83847ff50e2211ce6fed31
Vexus1k <vexus28@gmail.com>
2022-07-17 07:48:49 +0200

    avatar login

---
+++ src/app/home/pages/login-module/login-avatar/login-avatar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-avatar',
+  templateUrl: './login-avatar.component.html',
+  styleUrls: ['./login-avatar.component.css']
+})
+export class LoginAvatarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
